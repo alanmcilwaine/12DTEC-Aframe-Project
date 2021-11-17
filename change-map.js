78db9e3cec7299e198ee663be057d41b0681c65c
Alan <alan.j.mcilwaine@gmail.com>
2021-11-17 14:00:52 +1300

    #15 Added colour indicator for path button

--- change-map.js
+++ change-map.js
@@ -2,25 +2,34 @@ AFRAME.registerComponent("change-map", {
     init: function (){
         this.el.addEventListener("click", function(){
           let currentMap = document.querySelector("#mazeCall").getAttribute("src");
-          let changeMap = document.querySelector("#mazeCall")
-          let currentBox = this.getAttribute("class")
-          let changePath = document.querySelector("#pathCall")
+          let changeMap = document.querySelector("#mazeCall");
+          let currentBox = this.getAttribute("class");
+          let changePath = document.querySelector("#pathCall");
+          let changeText = document.querySelector("#difficultyValue");
+          let changeColor = document.querySelector("#pathButton");
+
           if (currentBox == "easy"){
               console.log("Changing scene from " + currentMap +  "=> Easy")
               console.log("Clicked Box: Easy")
               changeMap.setAttribute("src", "models/mazeEasy.gltf")
               changeMap.setAttribute("class", "easy")
               changePath.setAttribute("visible", false)
+              changeText.setAttribute("text", "value", "Easy")
+              changeColor.setAttribute("color", "red")
           }else if (currentBox == "med"){
               console.log(currentBox + " => Medium")
               changeMap.setAttribute("src", "models/mazeMed.gltf")
               changeMap.setAttribute("class", "med")
               changePath.setAttribute("visible", false)
+              changeText.setAttribute("text", "value", "Medium")
+              changeColor.setAttribute("color", "red")
           }else{
               console.log(currentBox + " => Hard")
               changeMap.setAttribute("src", "models/mazeHard.gltf")
               changeMap.setAttribute("class", "hard")
               changePath.setAttribute("visible", false)
+              changeText.setAttribute("text", "value", "Hard")
+              changeColor.setAttribute("color", "red")
           }
         })
     }
--- show-path.js
+++ show-path.js
@@ -3,31 +3,45 @@ AFRAME.registerComponent("show-path", {
         this.el.addEventListener("click", function(){
           let currentMap = document.querySelector("#mazeCall").getAttribute("class");
           let visiblePath = document.querySelector("#pathCall").getAttribute("visible");
-          let changePath = document.querySelector("#pathCall")
+          let changePath = document.querySelector("#pathCall");
+          let changeText = document.querySelector("#pathValue");
+          let changeColor = document.querySelector("#pathButton");
           if (currentMap == "easy" && visiblePath == true){
             console.log("Path => Hide Easy")
             changePath.setAttribute("visible", false)
             changePath.setAttribute("src", "models/mazeRouteEasy.gltf")
+            changeText.setAttribute("text", "value", "Off")
+            changeColor.setAttribute("color", "red")
           }else if (currentMap == "easy" && visiblePath == false){
             console.log("Path => Visible Easy")
             changePath.setAttribute("visible", true)
             changePath.setAttribute("src", "models/mazeRouteEasy.gltf")
+            changeText.setAttribute("text", "value", "On")
+            changeColor.setAttribute("color", "green")
           }else if (currentMap == "med" && visiblePath == true){
             console.log("Path => Hide Medium")
             changePath.setAttribute("visible", false)
             changePath.setAttribute("src", "models/mazeRouteMed.gltf")
+            changeText.setAttribute("text", "value", "Off")
+            changeColor.setAttribute("color", "red")
           }else if (currentMap == "med" && visiblePath == false){
             console.log("Path => Visible Medium")
             changePath.setAttribute("visible", true)
             changePath.setAttribute("src", "models/mazeRouteMed.gltf")
+            changeText.setAttribute("text", "value", "On")
+            changeColor.setAttribute("color", "green")
           }else if (currentMap == "hard" && visiblePath == true){
             console.log("Path => Hide Hard")
             changePath.setAttribute("visible", false)
             changePath.setAttribute("src", "models/mazeRouteHard.gltf")
+            changeText.setAttribute("text", "value", "Off")
+            changeColor.setAttribute("color", "red")
           }else if (currentMap == "hard" && visiblePath == false){
             console.log("Path => Visible Hard")
             changePath.setAttribute("visible", true)
             changePath.setAttribute("src", "models/mazeRouteHard.gltf")
+            changeText.setAttribute("text", "value", "On")
+            changeColor.setAttribute("color", "green")
           }
 
         })
