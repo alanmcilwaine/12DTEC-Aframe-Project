bd8ae85e7c84d83fe2115bd623408bc7a30c309b
Alan <18046am@hvhs.school.nz>
2021-11-17 23:36:25 +1300

    #21 Navmesh working with all difficulties

--- change-map.js
+++ change-map.js
@@ -9,25 +9,27 @@ AFRAME.registerComponent("change-map", {
           let changePath = document.querySelector("#pathCall");
           let changeText = document.querySelector("#difficultyValue");
           let changeColor = document.querySelector("#pathButton");
+          let changeMesh = document.querySelector("#meshCall");
 
-          function mapChange (src, mapClass, difficulty){
+          function mapChange (src, mapClass, difficulty, meshSrc){
             changeMap.setAttribute("src", src)
             changeMap.setAttribute("class", mapClass)
             changePath.setAttribute("visible", false)
             changeText.setAttribute("text", "value", difficulty)
             changeColor.setAttribute("color", "red")
+            changeMesh.setAttribute("src", meshSrc)
         }
 // Checks which box is clicked. (currentBox) class. 
 // Changes the map to whatever class was clicked 
           if (currentBox == "easy"){
               console.log("Changing scene from " + currentMap +  " => Easy")
-              mapChange("models/mazeEasy.gltf","easy", "Easy");
+              mapChange("models/mazeEasy.gltf","easy", "Easy", "models/easyMesh.gltf");
           }else if (currentBox == "med"){
               console.log("Changing scene from " + currentMap +  " => Medium")
-              mapChange("models/mazeMed.gltf","med", "Medium");
+              mapChange("models/mazeMed.gltf","med", "Medium", "models/medMesh.gltf");
           }else{
             console.log("Changing scene from " + currentMap +  " => Hard")
-              mapChange("models/mazeHard.gltf","hard", "Hard");
+              mapChange("models/mazeHard.gltf","hard", "Hard", "models/hardMesh.gltf");
           }
         })
     }
