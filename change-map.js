6639a7a61c3e70262c428339fbb8fd867a69429e
Alan <18046am@hvhs.school.nz>
2021-11-20 00:07:57 +1300

    Added a wireframe indicator to see what map is selected

--- change-map.js
+++ change-map.js
@@ -20,6 +20,12 @@ AFRAME.registerComponent("change-map", {
           let endTrophyRight = document.querySelector(".endTrophyRight");
           let endText = document.querySelector("#endText");
           let endDifficultyText = document.querySelector("#endDifficultyValue")
+          let easyButton = document.querySelector("#easyButton");
+          let medButton = document.querySelector("#medButton");
+          let hardButton = document.querySelector("#hardButton");
+          let easyButtonEnd = document.querySelector("#easyButtonEnd");
+          let medButtonEnd = document.querySelector("#medButtonEnd");
+          let hardButtonEnd = document.querySelector("#hardButtonEnd");
 
           function mapChange (src, mapClass, difficulty, meshSrc){
             changeMap.setAttribute("src", src)
@@ -42,20 +48,30 @@ AFRAME.registerComponent("change-map", {
             endTrophyLeft.setAttribute("position", leftTrophy)
             endTrophyRight.setAttribute("position", rightTrophy)
           }
-
+          function addWireframe (easy, med, hard){
+            easyButton.setAttribute("wireframe", easy)
+            medButton.setAttribute("wireframe", med)
+            hardButton.setAttribute("wireframe", hard)
+            easyButtonEnd.setAttribute("wireframe", easy)
+            medButtonEnd.setAttribute("wireframe", med)
+            hardButtonEnd.setAttribute("wireframe", hard)
+          }
 // Checks which box is clicked. (currentBox) class. 
 // Changes the map to whatever class was clicked 
           if (currentBox == "easy end endEasy" || currentBox == "easy"){
               console.log("Changing scene from " + currentMap +  " => Easy")
+              addWireframe(true, false, false)
               mapChange("models/mazeEasy.gltf","easy", "Current Map: Left:Easy", "models/easyMesh.gltf");
               positionEnd("0 4.25 -101.1", "-1.5 2.75 -100.5", "0 2.75 -100.5", "1.5 2.75 -100.5", "0 1 -101.1", "0 6 -101.1", "-3 -0.5 -80", "3 -0.5 -180");
 
           }else if (currentBox == "med end endMed" || currentBox == "med"){
               console.log("Changing scene from " + currentMap +  " => Medium")
+              addWireframe(false,true,false);
               mapChange("models/mazeMed.gltf","med", "Current Map: Middle:Medium", "models/medMesh.gltf");
               positionEnd("0 4.25 -145", "-1.5 2.75 -144", "0 2.75 -144", "1.5 2.75 -144", "0 1 -145", "0 6 -145", "-3 -0.5 -80", "3 -0.5 -80");
           }else{
             console.log("Changing scene from " + currentMap +  " => Hard")
+            addWireframe(false,false,true)
               mapChange("models/mazeHard.gltf","hard", "Current Map: Right:Hard", "models/hardMesh.gltf");
               positionEnd("0 4.25 -193", "-1.5 2.75 -192.5", "0 2.75 -192.5", "1.5 2.75 -192.5", "0 1 -193", "0 6 -193", "-3 -0.5 -170", "3 -0.5 -170");
 
