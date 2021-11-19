d1fd5ec1aa87069df912e6fd475e4bb10fa6f89f
Alan <18046am@hvhs.school.nz>
2021-11-20 01:32:18 +1300

    Text above music button now changes based on music

--- activate-music.js
+++ activate-music.js
@@ -4,21 +4,23 @@ AFRAME.registerComponent("activate-music", {
             let musicClass = document.querySelector("#music").getAttribute("class")
             let musicId = document.querySelector("#music");
             let entity = document.querySelector('[sound]');
+            let musicValue = document.querySelector("#musicValue")
 
+            function changeMusic(track, text, name){
+                musicId.setAttribute("sound", "src", track)
+                entity.components.sound.playSound();
+                musicValue.setAttribute("text", "value", text)
+                musicId.setAttribute("class", name)
+            }
             if (musicClass == "off"){
-                musicId.setAttribute("sound", "src", "music/gym.mp3")
-                entity.components.sound.playSound();
-                musicId.setAttribute("class", "gym")
+                changeMusic("music/gym.mp3", "Track (1/3): Gym", "gym")
             }else if(musicClass == "gym"){
-                musicId.setAttribute("sound", "src", "music/show.mp3")
-                entity.components.sound.playSound();
-                musicId.setAttribute("class", "show")
+                changeMusic("music/show.mp3", "Track (2/3): Show", "show")
             }else if (musicClass == "show"){
-                musicId.setAttribute("sound", "src", "music/happy.mp3")
-                entity.components.sound.playSound();
-                musicId.setAttribute("class", "happy")
+                changeMusic("music/happy.mp3", "Track (3/3): Happy", "happy")
             }else{
                 entity.components.sound.stopSound();
+                changeMusic("music/", "Track --", "off")
             }
         })
     }
